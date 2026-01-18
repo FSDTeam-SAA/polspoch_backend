f71941769a04bd318793c5576d3d2b08cd14cb36
jarifMohammed <mohammedjarif@protonmail.com>
2026-01-19 02:22:25 +0600

    Remove redundant family filter condition in getAllProducts method

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -63,10 +63,6 @@ export const ProductService = {
 
     const query: any = {}
 
-      // ✅ Apply family filter ONLY if it's not "all"
-  if (family && family !== "all") {
-    query.family = new Types.ObjectId(family)
-  }
 
     if (family) {
       query.family = new Types.ObjectId(family)
